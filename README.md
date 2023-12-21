1c8fe71ea234b53eea2d07a5d490fff0ea93f830
Jon Hurst <jon.a@hursts.org.uk>
2023-12-21 15:34:31 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AIMS mk 2
+
+The bare bones of extracting roster and freeform from an AIMS detailed
+roster.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aims",
+    version="0.0.1",
+    author="Jon Hurst",
+    author_email="jon.a@hursts.org.uk",
+    description="Extract information from AIMS detailed roster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+    ],
+    package_data={
+    },
+    entry_points={
+        "console_scripts": [
+            "aims2 = aims.main:main",
+        ]
+    },
+)
