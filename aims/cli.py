61065c89be69614e9d31bf19cdf0f2bf33bfd3fa
Jon Hurst <jon.a@hursts.org.uk>
2023-12-27 09:34:19 +0000

    Fixed line lengths

--- aims/cli.py
+++ aims/cli.py
@@ -9,7 +9,8 @@ import aims.output as output
 
 def _args():
     parser = argparse.ArgumentParser(
-        description='Process an AIMS detailed roster into various useful formats.')
+        description=(
+            'Process an AIMS detailed roster into various useful formats.'))
     parser.add_argument('format', choices=['roster', 'freeform', 'csv', 'ical'])
     parser.add_argument('--fo', action="store_true")
     return parser.parse_args()
