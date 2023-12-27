1bcdaab90845fe1dcfcfb5f97dee123b5800b2ea
Jon Hurst <jon.a@hursts.org.uk>
2023-12-27 12:43:58 +0000

    Moved name cleanup into output

--- aims/output.py
+++ aims/output.py
@@ -5,11 +5,40 @@ from zoneinfo import ZoneInfo as Z
 import io
 import csv as libcsv
 import datetime as dt
+import re
 
 from aims.roster import Duty, SectorFlags, CrewMember
-from aims.name_cleanup import clean
 import nightflight.night as nightcalc  # type: ignore
 from nightflight.airport_nvecs import airfields as nvecs  # type: ignore
+
+
+def clean(name: str) -> str:
+    parts = [X.strip().capitalize() for X in name.split()]
+    for c, part in enumerate(parts):
+        # remove bracketted stuff
+        if part[0] == "(":
+            parts[c] = ""
+            continue
+        # remove LR, SR, 50%
+        if part.upper() in {"LR", "SR", "50%"}:
+            parts[c] = ""
+            continue
+        # remove stars
+        new = re.sub(r"\*+", "", part)
+        # remove commas
+        new = new.replace(",", "")
+        # capitilize after - and '
+        for char in ("-", "'"):
+            index = new.find(char)
+            if index not in {-1, len(new) - 1}:
+                index += 1
+                new = new[:index] + new[index:].capitalize()
+        # capitilize after Mc
+        index = new.find("Mc")
+        if index == 0 and len(new) > 2:
+            new = new[:2] + new[2:].capitalize()
+        parts[c] = new
+    return " ".join([X for X in parts if X])
 
 
 def roster(duties: List[Duty]) -> str:
