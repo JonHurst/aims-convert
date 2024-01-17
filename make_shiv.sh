e3ea27627dec9bf68af23955faea6839e946a33e
Jon Hurst <jon.a@hursts.org.uk>
2024-01-17 19:23:15 +0000

    Updated script to make shiv

--- make_shiv.sh
+++ make_shiv.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 PROJ=/home/jon/proj/aims
+
+cd $PROJ
+python3 -m venv venv-shiv
+source venv-shiv/bin/activate
+pip install shiv
+pip install wheel
 shiv -c aimsgui -o aimsgui.pyw -p "/usr/bin/env python3" $PROJ
+deactivate
+rm -r venv-shiv
