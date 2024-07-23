a0995664631b47a834b8b1a22ca1acf67c500c79
Jon Hurst <jon.a@hursts.org.uk>
2024-07-23 18:01:06 +0100

    Started adding documentation to roster module

--- aims/roster.py
+++ aims/roster.py
@@ -1,3 +1,9 @@
+"""Processes the data extracted from a 'vertical' HTML AIMS roster.
+
+The module's public interface is the duties() function, which accepts "soup" as
+produced by processing the roster with BeautifulSoup.
+"""
+
 import datetime as dt
 from typing import Optional
 
@@ -63,6 +69,33 @@ def _sectors(data: Row, date: dt.date) -> tuple[Sector, ...]:
 
 
 def _duty(row: Row) -> Optional[Duty]:
+    """Process a stringified row of the HTML table into the Duty it represents.
+
+    Each row is a 12 item tuple. The first and last items are empty tuples
+    since the original table has empty cells at the beginning and end of the
+    row. In between these are tuples of strings; these come from splitting the
+    strings in the original cells on newlines.
+
+    The items in the row, from index 1 to index 10 are:
+
+        1:  Date
+        2:  Flight numbers plus aircraft type in square brackets or
+            quasi sector type code (e.g. standby, taxi, ...)
+        3:  Routes or description of quasi sector or all day duty
+        4:  Report time. This is not necessarily duty start time -- there may
+            be standby duties before report, and it is empty for standbys
+            without a call out.
+        5:  Sector times, including for quasi sectors. Empty for all day.
+        6:  Duty end time. Empty for standby without callout.
+        7:  Block hours
+        8:  Duty hours
+        9:  Markers for memos etc.
+        10: Crew list. This is one of the weaknesses of this format -- the crew
+            is associated with the entire duty rather than for each sector.
+
+    :param row: A 12 cell tuple, with each cell a tuple of strings.
+    :return: The Duty object represented by the row.
+    """
     try:
         if not row[CODES]:
             return None
@@ -95,6 +128,19 @@ def _duty(row: Row) -> Optional[Duty]:
 
 
 def duties(soup) -> tuple[Duty, ...]:
+    """Create a tuple of Duty objects from a BeautifulSoup of a roster.
+
+    The important part of the roster is in the form of a large HTML table, with
+    each row of the table representing a single duty. Whilst the header is a
+    date, if duties continue past midnight, those times are marked with a
+    superscript '+1' rather than being pushed to the next day, which makes it
+    much saner to parse than the alternatives.
+
+    :param soup: The soup of a 'vertical' HTML AIMS roster, as produced by
+               processing the HTML with BeautifulSoup.
+    :return: A tuple of Duty objects encoding the roster in a standard form.
+
+    """
     rows = iter(soup.find_all("tr"))
     try:
         while "Schedule Details" not in next(rows).stripped_strings:
