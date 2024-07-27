e074aeaff73575bc2549608ceb4515fb851ad420
Jon Hurst <jon.a@hursts.org.uk>
2024-07-27 11:53:25 +0100

    Added more tests for correct roster output

--- tests/test_output.py
+++ tests/test_output.py
@@ -57,6 +57,37 @@ standby_duty = Duty(
                quasi=True, position=False, crew=()), ))
 
 
+standby_before_flight = Duty(
+    code=None,
+    start=datetime.datetime(2023, 7, 2, 11, 45),
+    finish=datetime.datetime(2023, 7, 2, 19, 26),
+    sectors=(
+        Sector(name='LSBY', reg=None, type_=None,
+               from_='BRS', to='BRS',
+               off=datetime.datetime(2023, 7, 2, 11, 45),
+               on=datetime.datetime(2023, 7, 2, 13, 20),
+               quasi=True, position=False,
+               crew=()),
+        Sector(name='ADTY', reg=None, type_=None,
+               from_='BRS', to='BRS',
+               off=datetime.datetime(2023, 7, 2, 13, 20),
+               on=datetime.datetime(2023, 7, 2, 14, 0),
+               quasi=True, position=False,
+               crew=()),
+        Sector(name='227', reg=None, type_='319',
+               from_='BRS', to='BFS',
+               off=datetime.datetime(2023, 7, 2, 16, 14),
+               on=datetime.datetime(2023, 7, 2, 17, 23),
+               quasi=False, position=False,
+               crew=crew),
+        Sector(name='228', reg=None, type_='319',
+               from_='BFS', to='BRS',
+               off=datetime.datetime(2023, 7, 2, 17, 56),
+               on=datetime.datetime(2023, 7, 2, 18, 56),
+               quasi=False, position=False,
+               crew=crew)))
+
+
 class TestRoster(unittest.TestCase):
 
     def test_standard(self):
@@ -64,3 +95,10 @@ class TestRoster(unittest.TestCase):
             roster((standard_duty, all_day_duty, standby_duty)),
             "02/06/2023 06:00-17:01 BRS-LIS-BRS-NCL-BRS 7:02/11:01\n"
             "25/07/2024 06:15-14:15 ESBY 0:00/8:00")
+
+    def test_standby_before_flight(self):
+        self.assertEqual(
+            roster((standby_before_flight, )),
+            "02/07/2023 12:45-14:20 LSBY 0:00/1:35\n"
+            "02/07/2023 14:20-15:00 ADTY 0:00/0:40\n"
+            "02/07/2023 15:00-20:26 BRS-BFS-BRS 2:09/5:26")
