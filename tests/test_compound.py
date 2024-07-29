6b525f2ba5e87736370abb712a960c8270cfe473
Jon Hurst <jon.a@hursts.org.uk>
2024-07-29 07:58:54 +0100

    Added compound testing

---
+++ tests/test_compound.py
@@ -0,0 +1,28 @@
+import unittest
+from freezegun import freeze_time
+import glob
+
+import aims.parse
+import aims.output
+
+
+@freeze_time("2024-01-01")
+class Test_compound(unittest.TestCase):
+
+    def test_compound(self):
+        for f in glob.glob("files/*.htm?"):
+            with open(f) as input_:
+                duties, ade = aims.parse.parse(input_.read())
+            with open(f + ".txt") as roster_file:
+                self.assertEqual(roster_file.read(),
+                                 aims.output.roster(duties) + "\n")
+            with open(f + ".ical", "rb") as ical_file:
+                self.assertEqual(
+                    ical_file.read(),
+                    (aims.output.ical(duties, ade) + "\n").encode())
+            with open(f + ".csv", "rb") as csv_file:
+                self.assertEqual(csv_file.read(),
+                                 (aims.output.csv(duties) + "\n").encode())
+            with open(f + ".efj") as efj_file:
+                self.assertEqual(efj_file.read(),
+                                 aims.output.efj(duties) + "\n")
